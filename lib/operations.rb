884c38bca3f17b56448977e26256def3a45bb2ff
Linda Chang <lindachang186@gmail.com>
2018-10-04 20:51:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 def unsafe?(speed)
-	if speed > 60 && speed < 40 
-		return false 
-	else speed < 60 && speed > 40 
+	if speed > 60 && speed < 40
+		return false
+	else speed < 60 && speed > 40
 		return true
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
